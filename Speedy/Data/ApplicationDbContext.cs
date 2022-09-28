aa108240f9237bd1566984282732f259f63ba7f5
DimitarShumanov <shumanovd125@gmail.com>
2022-09-28 10:35:11 +0300

    Add project files.

---
+++ Speedy/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Speedy.Models;
+namespace Speedy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BorrowLend> BorrowLend { get; set; }
+    }
+}
---
+++ Speedy/Models/BorrowLend.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Speedy.Models
+{
+    [Table("BorrowLend")]
+    public class BorrowLend
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        public string Borrower { get; set; }
+        [Required]
+        public string Lender { get; set; }
+    }
+}
