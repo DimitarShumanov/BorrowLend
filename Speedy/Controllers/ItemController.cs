601630f630dd8bf0d3a17693e6857e049ea44b24
DimitarShumanov <shumanovd125@gmail.com>
2022-09-28 12:05:16 +0300

    part1

---
+++ Speedy/Controllers/ItemController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Speedy.Data;
+using Speedy.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Speedy.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<BorrowLend> obj = _db.BorrowLend;
+            return View(obj);
+        }
+    }
+}
